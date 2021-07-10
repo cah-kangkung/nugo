dd1d78c052feca70697fa12cabc9c3c822fe5497
cah-kangkung <hafizhun.alim93@gmail.com>
2021-07-10 09:26:16 +0700

    Initial commit

---
+++ app/src/main/java/com/hafiz_1313617032_uas/nugo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hafiz_1313617032_uas.nugo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
