8ea1bf9d6f65171b52a69afe5b8728f7e65f3c83
cah-kangkung <hafizhun.alim93@gmail.com>
2021-07-10 10:18:42 +0700

    Implement bottom nav

--- app/src/main/java/com/hafiz_1313617032_uas/nugo/MainActivity.java
+++ app/src/main/java/com/hafiz_1313617032_uas/nugo/MainActivity.java
@@ -1,14 +1,63 @@
 package com.hafiz_1313617032_uas.nugo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hafiz_1313617032_uas.nugo.Fragment.CalculatorFragment;
+import com.hafiz_1313617032_uas.nugo.Fragment.DailyRecipeFragment;
+import com.hafiz_1313617032_uas.nugo.Fragment.HomeFragment;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //loading the default fragment
+        loadFragment(new HomeFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+
+            case R.id.navigation_calorie_calculator:
+                fragment = new CalculatorFragment();
+                break;
+
+            case R.id.navigation_daily_recipe:
+                fragment = new DailyRecipeFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
     }
 }
